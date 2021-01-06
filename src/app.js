6237c4434deffa66ea2a6018880e015f2af6166c
Abhi <abhijitjoshi27601@gmail.com>
2021-01-06 12:07:14 +0530

    changes for packag.json

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ const renderApp =() => {
 
 firebase.auth().onAuthStateChanged((user) => {
     if(user) {
-        console.log(user.uid)
+       // console.log(user.uid)
         store.dispatch(login(user.uid))
         store.dispatch(startSetFlights()).then(() => {
             renderApp()
