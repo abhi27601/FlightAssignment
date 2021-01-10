3620e62b83db43239c306363d6543de43ade4cce
Abhi <abhijitjoshi27601@gmail.com>
2021-01-10 17:03:54 +0530

    added compression for server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const compression = require('compression')
 const app = express();
 const path = require('path')
 const publicPath= path.join(__dirname,'..','public','dist')
-console.log(__dirname);
-console.log(publicPath);
 const PORT = process.env.PORT || 3000;
 console.log(PORT);
+app.use(compression());
 app.use(express.static(publicPath));
 app.get('*',(req,res) => {
     res.sendFile(path.join(publicPath,'index.html'))
